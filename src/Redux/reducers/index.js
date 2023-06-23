5db0699183ac1a010fc737c86c62c13096b39928
BrunoSouzaFarias <d2fake@gmail.com>
2023-06-23 17:29:04 -0300

    Feat(Login) Arrumado os requisitos 1,2,3,4

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import playerReducer from './playerNameReducer';
 import gravatarReducer from './gravatarReducer';
+import user from './user';
 
 const rootReducer = combineReducers({
   gravatar: playerReducer,
   playerName: gravatarReducer,
+  user,
 });
 
 export default rootReducer;
